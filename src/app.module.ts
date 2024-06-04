d07db9f00d85875f8210106fb7a765f73c1bbb12
Cristian Varela Casas <cristianchantada@gmail.com>
2024-06-04 17:29:50 +0200

    Configuración clase 381 para subir a Railway

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.Mongo_URI, {
+      dbName: process.env.MONGO_DB_NAME,
+    }),
     AuthModule,
-    MongooseModule.forRoot(),
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(){
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,107 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './entities/user.entity';
+import * as bcryptjs from 'bcryptjs';
+import { CreateUserDto, LoginDto, RegisterUserDto, UpdateAuthDto } from './dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt-payload';
+import { LoginResponse } from './interfaces/login-response';
+
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+
+ 
+  constructor(
+    @InjectModel(User.name) 
+    private userModel: Model<User>,
+    private jwtService: JwtService
+  ){}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+
+    try {
+      const {password, ...userData} = createUserDto;
+      // 1.- Encriptar contraseña
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...userData
+      });
+      
+    // 2.- Guardar el usuario
+    await newUser.save();
+    
+    const {password: _, ...user} = newUser.toJSON();
+     return user;
+      
+    } catch(error){
+      console.log(error.code);
+      if(error.code === 11000){
+        throw new BadRequestException(`${ createUserDto } already exists !`);
+      }
+      throw new InternalServerErrorException('Something terrible happen !!!')
+    }
+    
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async register(registerUserdto: RegisterUserDto): Promise<LoginResponse> {
+      const user= await this.create(registerUserdto);
+      return {
+        user,
+        token: this.getJwtToken({id: user._id})
+      }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
+
+  async login(loginDto :LoginDto): Promise<LoginResponse>{
+
+    const {email, password} = loginDto;
+    const user = await this.userModel.findOne({email});
+
+    if(!user){
+      throw new UnauthorizedException('Not valid credentials - email')
+    }
+    
+    if(!bcryptjs.compareSync(password, user.password)){
+      throw new UnauthorizedException('Not valid credentials - password')
+    }
+
+    const {password: _, ...rest} = user.toJSON();
+
+    return {
+      user: rest,
+      token: this.getJwtToken({id: user.id})
+    };
+
   }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
+  findAll(): Promise<User[]> {
+    return this.userModel.find();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  async findUserById(id: string){
+    const user = await this.userModel.findById(id);
+    const {password, ...rest} = user.toJSON();
+    return rest;
   }
+
+  // findOne(id: number) {
+  //   return `This action returns a #${id} auth`;
+  // }
+
+  // update(id: number, updateAuthDto: UpdateAuthDto) {
+  //   return `This action updates a #${id} auth`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} auth`;
+  // }
+
+  getJwtToken(payload: JwtPayload){
+    const token = this.jwtService.sign(payload);
+    return token;
+  }
+
+
 }
